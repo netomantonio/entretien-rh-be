0b05fe22fce1a469af5f5d6911dc38220cc5d67c
Leonardo Wrobel <leonardo.wrobel@tuntscorp.com>
2022-12-20 11:20:56 -0300

    (fix): adjustments on fruit entity

--- src/main/kotlin/br/ufpr/tcc/entretien/backend/model/Fruit.kt
+++ src/main/kotlin/br/ufpr/tcc/entretien/backend/model/Fruit.kt
@@ -3,18 +3,9 @@ package br.ufpr.tcc.entretien.backend.model
 import java.time.Instant
 import javax.persistence.*
 
-@Entity
 @Table(name = "fruit")
-class Fruit {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private val id: Long? = null
-
-    @Column(name = "name")
-    lateinit var name: String
-
-    @Column(name = "createdAt")
-    private val createdAt = Instant.now()
-}
+@Entity
+class Fruit(
+    @Column(name = "name") val name: String,
+    @Column(name = "createdAt") var createdAt: Instant = Instant.now()
+) : AbstractJpaPersistable<Long>()
