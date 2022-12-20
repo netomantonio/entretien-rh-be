a36cdd24192be0230952f6b3e2334e11360e1bea
Leonardo Wrobel <leonardo.wrobel@tuntscorp.com>
2022-12-20 11:51:05 -0300

    (fix): unique name

--- src/main/kotlin/br/ufpr/tcc/entretien/backend/model/Fruit.kt
+++ src/main/kotlin/br/ufpr/tcc/entretien/backend/model/Fruit.kt
@@ -6,6 +6,6 @@ import javax.persistence.*
 @Table(name = "fruit")
 @Entity
 class Fruit(
-    @Column(name = "name") val name: String,
+    @Column(unique = true, name = "name") val name: String,
     @Column(name = "createdAt") var createdAt: Instant = Instant.now()
 ) : AbstractJpaPersistable<Long>()
