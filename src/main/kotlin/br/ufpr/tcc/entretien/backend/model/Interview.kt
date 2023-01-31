54a2a040c7921cedbfe0c89adb5cac8a05f8c36e
Leonardo Wrobel <leonardo.wrobel@tuntscorp.com>
2023-01-31 19:02:13 -0300

    feat(): schedule/interview entities;

--- src/main/kotlin/br/ufpr/tcc/entretien/backend/model/Interview.kt
+++ src/main/kotlin/br/ufpr/tcc/entretien/backend/model/Interview.kt
@@ -1,6 +1,5 @@
 package br.ufpr.tcc.entretien.backend.model
 
-import br.ufpr.tcc.entretien.backend.model.enums.EDayOfTheWeek
 import br.ufpr.tcc.entretien.backend.model.infra.AbstractJpaPersistable
 import br.ufpr.tcc.entretien.backend.model.users.Candidate
 import br.ufpr.tcc.entretien.backend.model.users.Recruiter
@@ -8,16 +7,17 @@ import java.time.Instant
 import java.util.*
 import javax.persistence.*
 
-/**
- * Entity class that models Schedule-specific attributes.
- */
 @Entity
-class Schedule(
+class Interview(
     @ManyToOne
     @JoinColumn(name = "fk_recruiter")
     var recruiter: Recruiter,
+    @ManyToOne
+    @JoinColumn(name = "fk_candidate")
+    var candidate: Candidate,
     @Temporal(TemporalType.TIMESTAMP) val createdAt: Date = Date.from(Instant.now()),
     @Temporal(TemporalType.TIMESTAMP) val updatedAt: Date = Date.from(Instant.now()),
-    @Enumerated(EnumType.ORDINAL)
-    var dayOfTheWeek: EDayOfTheWeek
+    @Temporal(TemporalType.TIMESTAMP) val startingAt: Date = Date.from(Instant.now()),
+    @Temporal(TemporalType.TIMESTAMP) val endingAt: Date = Date.from(Instant.now()),
+    var estimatedDuration: Long? = null
 ) : AbstractJpaPersistable<Long>()
---
+++ src/main/kotlin/br/ufpr/tcc/entretien/backend/model/enums/EDayOfTheWeek.kt
@@ -0,0 +1,11 @@
+package br.ufpr.tcc.entretien.backend.model.enums
+
+enum class EDayOfTheWeek(val value: Int) {
+    SUNDAY(0),
+    MONDAY(1),
+    TUESDAY(2),
+    WEDNESDAY(3),
+    THURSDAY(4),
+    FRIDAY(5),
+    SATURDAY(6)
+}
