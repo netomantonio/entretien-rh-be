c07d2208854a8b8c51b30347e3344aa7bd0ee9b7
Leonardo Wrobel <leonardo.wrobel@tuntscorp.com>
2023-02-01 15:59:14 -0300

    feat(): interview class; register manager;

--- src/main/kotlin/br/ufpr/tcc/entretien/backend/model/Interview.kt
+++ src/main/kotlin/br/ufpr/tcc/entretien/backend/model/Interview.kt
@@ -3,6 +3,7 @@ package br.ufpr.tcc.entretien.backend.model
 import br.ufpr.tcc.entretien.backend.model.enums.EInterviewStatus
 import br.ufpr.tcc.entretien.backend.model.infra.AbstractJpaPersistable
 import br.ufpr.tcc.entretien.backend.model.users.Candidate
+import br.ufpr.tcc.entretien.backend.model.users.Manager
 import br.ufpr.tcc.entretien.backend.model.users.Recruiter
 import java.time.Instant
 import java.util.*
@@ -20,6 +21,9 @@ class Interview(
     @ManyToOne
     @JoinColumn(name = "fk_candidate")
     var candidate: Candidate,
+    @ManyToOne
+    @JoinColumn(name = "fk_manager")
+    var manager: Manager,
     @Temporal(TemporalType.TIMESTAMP) val createdAt: Date = Date.from(Instant.now()),
     @Temporal(TemporalType.TIMESTAMP) val updatedAt: Date = Date.from(Instant.now()),
     @Temporal(TemporalType.TIMESTAMP) val startingAt: Date = Date.from(Instant.now()),
---
+++ src/main/kotlin/br/ufpr/tcc/entretien/backend/model/users/Manager.kt
@@ -0,0 +1,15 @@
+package br.ufpr.tcc.entretien.backend.model.users
+
+import javax.persistence.*
+import javax.validation.constraints.NotBlank
+
+/**
+ * Entity class that models Manager-specific attributes.
+ */
+@Entity
+class Manager(
+    @NotBlank var companyName: String = "",
+    var tradingName: String? = null,
+    @NotBlank var cnpj: String = "",
+    var operationArea: String? = null
+) : User()
