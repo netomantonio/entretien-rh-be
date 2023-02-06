5a7bb459b8d9d579f2dd1e6b343879672f64f2ae
Leonardo Wrobel <leonardo.wrobel@tuntscorp.com>
2023-02-06 15:19:51 -0300

    feat(): adjustments;

--- src/main/kotlin/br/ufpr/tcc/entretien/backend/controller/InterviewController.kt
+++ src/main/kotlin/br/ufpr/tcc/entretien/backend/controller/InterviewController.kt
@@ -2,9 +2,11 @@ package br.ufpr.tcc.entretien.backend.controller
 
 import br.ufpr.tcc.entretien.backend.datasource.request.InterviewRequest
 import br.ufpr.tcc.entretien.backend.service.InterviewService
+import br.ufpr.tcc.entretien.backend.service.UserDetailsImpl
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
@@ -18,10 +20,27 @@ class InterviewController {
 
     @PreAuthorize("hasRole('ROLE_MANAGER')")
     @PostMapping("")
-    fun createNewInterview(@Valid @RequestBody interviewRequest: InterviewRequest): ResponseEntity<*> {
+    fun createNewInterview(@Valid @RequestBody interviewRequest: InterviewRequest, authentication: Authentication): ResponseEntity<*> {
+        val userDetails: UserDetailsImpl = authentication.principal as UserDetailsImpl
+
+        val managerId = userDetails.getId()
         return try {
-            interviewService.createInterview(interviewRequest)
+            interviewService.createInterview(interviewRequest, managerId)
             ResponseEntity.ok<Any>("Interview registered successfully!")
+        } catch (ex: Exception) {
+            println("[ERROR] ------------------------------------------")
+            println(ex.message)
+            ex.printStackTrace()
+            ResponseEntity.internalServerError().body("Persistence error.")
+        }
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("")
+    fun getAllInterviews(): ResponseEntity<*> {
+        return try {
+            val interviews = interviewService.getAll()
+            ResponseEntity.ok<Any>(interviews)
         } catch (ex: Exception) {
             println("[ERROR] ------------------------------------------")
             println(ex.message)
